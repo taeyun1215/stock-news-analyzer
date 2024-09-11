6e6ce497b70eb9472f0804080feb4be8f5b412cc
taeyun1215 <taeyun1215@naver.com>
2024-09-11 21:12:36 +0900

    post controller

--- src/main/java/com/example/demo/example/PostController.java
+++ src/main/java/com/example/demo/example/PostController.java
@@ -1,10 +1,9 @@
 package com.example.demo.example;
 
 import net.gpedro.integrations.slack.SlackApi;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,8 +14,39 @@ public class PostController {
 
 	private final SlackApi slackApi;
 
-	@GetMapping("/example")
-	public void example(@RequestBody ExampleRequest exampleRequest) {
-		throw new ExampleException("예시 에러");
+	// GET all posts
+	@GetMapping
+	public List<PostResponse> getAllPosts() {
+		return postService.getAllPosts();
+	}
+
+	// GET post by ID
+	@GetMapping("/{id}")
+	public ResponseEntity<PostResponse> getPostById(@PathVariable Long id) {
+		return postService.getPostById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	// GET post by title (query parameter)
+	@GetMapping("/search")
+	public List<PostResponse> getPostsByTitle(@RequestParam String title) {
+		return postService.getPostsByTitle(title);
+	}
+
+	// POST create a new post
+	@PostMapping
+	public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest postRequest) {
+		return new ResponseEntity<>(postService.createPost(postRequest), HttpStatus.CREATED);
+	}
+
+	// DELETE post by ID
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+		if (postService.deletePost(id)) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 }
