13c507686958e92aff30822d318c478b2f47418a
taeyun1215 <taeyun1215@naver.com>
2024-09-19 21:42:30 +0900

    sample entity

--- src/main/java/com/example/demo/example/Sample.java
+++ src/main/java/com/example/demo/example/Sample.java
@@ -1,4 +1,18 @@
 package com.example.demo.example;
 
-public class Sample {
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+class Sample {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
 }
