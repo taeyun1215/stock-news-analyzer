45f93477e1dd510648747a4e1df4841add5ccd11
taeyun1215 <taeyun1215@naver.com>
2024-10-23 17:32:02 +0900

    git cache del

--- build/resources/main/db/migration/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO SAMPLE (name, description) VALUES ('Sample 1', 'Description 1');
-INSERT INTO SAMPLE (name, description) VALUES ('Sample 2', 'Description 2');
-INSERT INTO SAMPLE (name, description) VALUES ('Sample 3', 'Description 3');
-INSERT INTO SAMPLE (name, description) VALUES ('Sample 4', 'Description 4');
-INSERT INTO SAMPLE (name, description) VALUES ('Sample 5', 'Description 5');
